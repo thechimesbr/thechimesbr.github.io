adde479ab0124a716ae03fab9a193917463894c8
ian adams <ijadams503@gmail.com>
2017-08-27 18:51:11 -0500

    start work on homepage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      speed: 1000,
+      pause: 7500,
+      controls: false,
+      auto: true,
+      autoControls: false,
+      pager: false,
+      easing: 'swing',
+    });
 });

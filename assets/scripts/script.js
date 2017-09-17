80ed42fd9e40747f877ba28926b385f00cc67bb0
ian adams <ijadams503@gmail.com>
2017-09-17 14:08:20 -0500

    add images and contact form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
       easing: 'swing',
     });
     $('nav ul').slicknav();
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
